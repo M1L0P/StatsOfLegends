91db2d3dacb1ac77569253d12e5ac58dbd6d1c39
Kian Gribi <kian.gribi@edu.tbz.ch>
2019-05-10 10:40:03 +0200

    Finished GUI for match history started working on API

---
+++ app/src/main/java/ch/noseryoung/statsoflegends/persistence/StaticManager.kt
@@ -0,0 +1,31 @@
+package ch.noseryoung.statsoflegends.persistence
+
+import android.graphics.Bitmap
+
+object StaticManager {
+
+    fun getChampion(id: Int) : Bitmap? {
+
+    }
+
+    fun getItem(id: Int) : Bitmap? {
+
+    }
+
+    fun getPerk(id: Int) : Bitmap? {
+
+    }
+
+    // Check if an image already exists in cache
+    private fun get(id: Int) {
+
+    }
+
+    private fun getOnline(id: Int) : Bitmap {
+
+    }
+
+    private fun getLocal(id: Int) : Bitmap {
+
+    }
+}
